22acefd8485f28697f0b6afda43e95be936f02e9
Yidan Yang <39606635+yidan-yang@users.noreply.github.com>
2020-09-09 13:50:45 -0400

    Create buildcalculator.cpp

---
+++ buildcalculator.cpp
@@ -0,0 +1,20 @@
+// 9/9/2020
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double num1, num2;
+	cout << "Enter first number: ";
+	cin >> num1; 
+	cout << "Enter second number: ";
+	cin >> num2;
+
+	cout << "The sum of 1st and 2nd numbers are: " << num1 + num2;
+
+	return 0;
+
+}
