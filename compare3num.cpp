ac62a288a1fd645522bac6867a38ccde6ba70234
Yidan Yang <39606635+yidan-yang@users.noreply.github.com>
2020-09-09 22:24:23 -0400

    Create compare3num.cpp

---
+++ compare3num.cpp
@@ -0,0 +1,24 @@
+/* comparing 3 nums*/
+
+#include <iostream>
+using namespace std;
+
+int getMax(int num1, int num2, int num3) {
+	int result;
+
+	if (num1 >= num2 && num1>= num3) {
+		result = num1;
+	}
+	else if (num2 >= num1 && num2 >= num3) {
+		result = num2;
+	}
+	else {
+		result = num3;
+	}
+	return result;
+}
+
+int main() {
+	cout << getMax(0, 3, 5);
+	return 0;
+}
