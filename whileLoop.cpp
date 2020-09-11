b2863a6069a1bf7139e75837a4e3946371c7353d
Yidan Yang <39606635+yidan-yang@users.noreply.github.com>
2020-09-10 20:21:36 -0400

    Create whileLoop.cpp

---
+++ whileLoop.cpp
@@ -0,0 +1,20 @@
+/* 9/10/2020 */
+/* while loop */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int index = 0;
+	while (index <= 5) {
+		//cout << index << endl;
+		index++;
+		//cout << index << endl;
+	}
+	cout << index << endl;
+	return 0;
+}
+
+
+/* note: there is a difference between where the print function*/
