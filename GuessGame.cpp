7cab6fb5d33533667d22765e2f70d9550cfad144
Yidan Yang <39606635+yidan-yang@users.noreply.github.com>
2020-09-11 14:00:03 -0400

    Update GuessGame.cpp

--- GuessGame.cpp
+++ GuessGame.cpp
@@ -35,3 +35,33 @@ int main() {
 
 	return 0;
 }
+
+
+/* impose a guess limit*/
+int main() {
+
+	int secretNum = 7;
+	int guess{};  /* define a input variable*/
+	int guessLimit = 3;
+	int guessCount = 0;
+	bool outOfGuesses = false;
+
+	while (secretNum != guess && !outOfGuesses) {
+		if (guessCount < guessLimit) {
+			cout << "Enter guess: ";
+			cin >> guess;
+			guessCount++;
+		}
+		else { 
+			outOfGuesses = true;
+		}
+	}
+	if (outOfGuesses == true) {
+		cout << "You lose!";
+	}
+	else {
+		cout << "You win!";
+	}
+
+}
+
