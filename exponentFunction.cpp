cc8d13bcee316ab9e6cc063a2c3ca13dff312c79
Yidan Yang <39606635+yidan-yang@users.noreply.github.com>
2020-09-11 15:52:32 -0400

    Create exponentFunction.cpp

---
+++ exponentFunction.cpp
@@ -0,0 +1,43 @@
+/* 9/11/2020 */
+/* exponent function */
+
+#include <iostream>
+using namespace std;
+
+int power(int baseNum, int powNum) {
+	int result = 1;
+	for (int i = 0; i < powNum; i++) {
+		result = result * baseNum;
+	}
+	return result;
+}
+
+
+int main() {
+	cout << power(2, 3);
+	return 0;
+}
+
+
+
+/* enter number interface*/
+
+
+int main() {
+
+	int baseNum;
+	int powNum;
+	int result = 1;
+
+	cout << "Enter base number: " ;
+	cin >> baseNum;
+	cout << "Enter power number: " ;
+	cin >> powNum;
+
+	for (int i = 0; i < powNum; i++) {
+		result = result * baseNum;
+	}
+	
+	cout << "Final answer is : " << result << endl;
+	return 0;
+}
